ed91335ae2556ef3ee251e17b2b787c2a924a4f6
Thamashwerago <62113345+Thamashwerago@users.noreply.github.com>
2024-03-08 00:07:12 +0530

    Add Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:hostel_hive/pages/sign_in_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+/*await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);*/
 
 void main() {
   runApp(const MyApp());
