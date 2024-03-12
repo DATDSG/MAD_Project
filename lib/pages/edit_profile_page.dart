bf9b9d06970d385afde59e9320251c745da750f7
AshiniWeerabahu <109893681+AshiniWeerabahu@users.noreply.github.com>
2024-03-12 13:21:06 +0530

    UI update
    Edit Profile Page

--- lib/pages/edit_profile_page.dart
+++ lib/pages/edit_profile_page.dart
@@ -34,6 +34,405 @@ class _EditProfilePageState extends State<EditProfilePage> {
           ],
         ),
       ),
+      body:ListView(
+        children: [
+          Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+          child: Column(
+            children: [
+              const Center(
+                child: CircleAvatar(
+                  radius: 60,
+                  backgroundImage: AssetImage('assets/images/profile.jpg'),
+                ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.only(bottom: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Icon(Icons.edit, size: 15,),
+                    GestureDetector(
+                      onTap: () {},
+                      child: const Text(
+                        'Edit',
+                        style: TextStyle(
+                          fontSize: 15,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              // Name
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 5),
+                child: Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.1),
+                        spreadRadius: 1,
+                        blurRadius: 2,
+                        offset: const Offset(0, 2),
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.only(top: 10, left: 20),
+                        child: Text(
+                          "Name",
+                          style: TextStyle(
+                            fontSize: 17,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 20, bottom: 10),
+                        child: SizedBox(
+                          height: 25,
+                          child: Padding(
+                            padding: const EdgeInsets.only(right: 10),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                hintText: 'Ishara Uditha',
+                                hintStyle: TextStyle(
+                                  fontSize: 15,
+                                  color: Colors.grey[700],
+                                ),
+                                border: const UnderlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: Colors.grey,
+                                    width: 2,
+                                  ),
+                                ),
+                                contentPadding:
+                                    const EdgeInsets.only(bottom: 10),
+                              ),
+                              style: TextStyle(
+                                fontSize: 15,
+                                color: Colors.grey[700],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+              // Email address
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 5),
+                child: Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.1),
+                        spreadRadius: 1,
+                        blurRadius: 2,
+                        offset: const Offset(0, 2),
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.only(top: 10, left: 20),
+                        child: Text(
+                          "Email Address",
+                          style: TextStyle(
+                            fontSize: 17,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 20, bottom: 10),
+                        child: SizedBox(
+                          height: 25,
+                          child: Padding(
+                            padding: const EdgeInsets.only(right: 10),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                hintText: 'uditha@gmail.com',
+                                hintStyle: TextStyle(
+                                  fontSize: 15,
+                                  color: Colors.grey[700],
+                                ),
+                                
+                                border: const UnderlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: Colors.grey,
+                                    width: 2,
+                                  ),
+                                ),
+                                contentPadding:
+                                    const EdgeInsets.only(bottom: 10),
+                              ),
+                              style: TextStyle(
+                                fontSize: 15,
+                                color: Colors.grey[700],
+                              ),
+                              keyboardType: TextInputType.emailAddress,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+              // Contact Number
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 5),
+                child: Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.1),
+                        spreadRadius: 1,
+                        blurRadius: 2,
+                        offset: const Offset(0, 2),
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.only(top: 10, left: 20),
+                        child: Text(
+                          "Contact Number",
+                          style: TextStyle(
+                            fontSize: 17,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 20, bottom: 10),
+                        child: SizedBox(
+                          height: 25,
+                          child: Padding(
+                            padding: const EdgeInsets.only(right: 10),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                hintText: '0761516278',
+                                hintStyle: TextStyle(
+                                  fontSize: 15,
+                                  color: Colors.grey[700],
+                                ),
+                                border: const UnderlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: Colors.grey,
+                                    width: 2,
+                                  ),
+                                ),
+                                contentPadding:
+                                    const EdgeInsets.only(bottom: 10),
+                              ),
+                              style: TextStyle(
+                                fontSize: 15,
+                                color: Colors.grey[700],
+                              ),
+                              keyboardType: TextInputType.number,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+              // Save button
+              Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: SizedBox(
+                  height: 45,
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const EditProfilePage(),
+                        ),
+                      );
+                    },
+                    style: ButtonStyle(
+                      shadowColor: MaterialStateProperty.all(Colors.grey),
+                      backgroundColor: MaterialStateProperty.all(
+                        Colors.green[400],
+                      ),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                    child: const Text(
+                      'Save',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+
+              // Change Password
+              const Padding(
+                padding: EdgeInsets.only(top: 10, bottom: 10),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Change Password',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              // New Password
+              TextField(
+                controller: newPasswordController,
+                obscureText: isObscure,
+                decoration: InputDecoration(
+                  contentPadding: const EdgeInsets.all(8),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: const BorderSide(
+                      color: Colors.green,
+                      width: 2,
+                    ),
+                  ),
+                  prefixIcon: Icon(
+                    Icons.lock,
+                    size: 18,
+                    color: Colors.grey[400],
+                  ),
+                  suffixIcon: GestureDetector(
+                    onTap: () => setState(() {
+                      isObscure = !isObscure;
+                    }),
+                    child: Icon(
+                      isObscure ? Icons.visibility_off : Icons.visibility,
+                      size: 18,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  hintText: 'New Password',
+                  hintStyle: TextStyle(
+                    color: Colors.grey[400],
+                  ),
+                ),
+              ),
+
+              const SizedBox(
+                height: 10,
+              ),
+              // Confirm New Password
+              TextField(
+                controller: confirmNewPasswordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  contentPadding: const EdgeInsets.all(8),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(
+                      color: isPasswordMatch ? Colors.green : Colors.red,
+                      width: 2,
+                    ),
+                  ),
+                  prefixIcon: Icon(
+                    Icons.lock,
+                    size: 18,
+                    color: Colors.grey[400],
+                  ),
+                  hintText: 'Confirm New Password',
+                  hintStyle: TextStyle(
+                    color: Colors.grey[400],
+                  ),
+                ),
+                onChanged: (value) => setState(() {
+                  isPasswordMatch = value == newPasswordController.text;
+                }),
+              ),
+
+              // Save button
+              Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: SizedBox(
+                  height: 45,
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const EditProfilePage(),
+                        ),
+                      );
+                    },
+                    style: ButtonStyle(
+                      shadowColor: MaterialStateProperty.all(Colors.grey),
+                      backgroundColor: MaterialStateProperty.all(
+                        Colors.green[400],
+                      ),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                    child: const Text(
+                      'Save',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        ],
+      ),
     );
   }
 }
